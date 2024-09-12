b477d299ce36ea2784229eabef817704e96c1438
eduarda vasconcelos <eduardavasconcelos145@gmail.com>
2024-09-12 20:32:42 -0300

    exercício 1

---
+++ exercicios/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Greeting from './components/greeting'
+import './App.css'
+
+function App() {
+
+  return (
+  
+    <div>
+      <Greeting name ="Fulano"/>
+    </div>
+  )
+}
+
+export default App
+
---
+++ exercicios/src/components/greeting.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+function Greeting({name}) {
+  return <h1>Olá, {name}!</h1>;
+}
+
+export default Greeting;
+
